594af673fabf42d3ab0834911f6daf716b37a631
VictoriqueMoe <loli@victorique.moe>
2025-10-08 11:46:26 +0100

    fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,28 @@
 // @ts-check
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from "typescript-eslint";
 import eslintJs from "@eslint/js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
 const config = [
-    { ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"] },
+    { ignores: [".next/**"] },
     eslintJs.configs.recommended,
     ...tseslint.configs.recommended,
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    },
     {
         rules: {
             curly: ["error", "all"],
-            "@typescript-eslint/no-unused-vars": "warn",
-            "@typescript-eslint/no-explicit-any": "warn",
         },
     },
 ];
